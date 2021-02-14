73f1c98403380ea9816c0e7ed149135b8d473209
nk17232 <75089041+nk17232@users.noreply.github.com>
2021-02-13 22:27:00 -0600

    Add files via upload

---
+++ paperClass.js
@@ -0,0 +1,27 @@
+class paperClass{
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: false,
+        friction:0.5,
+        density:1.2
+
+      }
+        this.image =loadImage('sprites/paper.png'); 
+        this.bodies=rect(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      };
+
+
+      display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height)
+        pop();
+      }
+    }
